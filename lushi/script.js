314305ba44976b144344962c4385db2d17c978eb
l0vest0rm <test@gmail.com>
2025-06-16 07:24:24 +0800

    s

---
+++ lushi/script.js
@@ -0,0 +1,187 @@
+//黑暗之赐
+const darkness= [
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/48673e9a2c56038b8cd6f1f979bf4c3973c7bccfd42b918fa5a7b7e3264f8689.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/a16c7978e016843634da02a0091c828c0b523bc3c6e4115b3ecea1b6a12ccd58.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/f577f47f2fd366469103a058050cd2fa8b65c41707a6b2dfe4c8612658c6ab61.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/183a3186524bef449ccbf3de5398f4dd8ed0e3aaaebb793e7ab3491fa598411e.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/6f5f4a347b2387d44d95053362451ab4bfaca6e1eb3f5d346a03deaa9938e820.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/c42c9437c10435af16fc5d8064e0c488508c9b4e91c29db17629d2738864cb3a.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/936a40575d3830050bbc08767624c1da3a8b4acad5fe4e4e40fc5f0369c6e940.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/ddae937ecf249cca41dfaa5452f16f2c872524e73356d3ee6dbb01441ae2e80d.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/3a4c16bf85a9b46fd593a6725ba65a10db6298c77f16d6a7b4947e1fb17ccc23.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/07bd82bb28ff32c808ac3697f9d0be44150f8e9eb209957c790142394c7c49cc.png'
+];
+
+//奥秘
+const mystery = [
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/d4057b18002de297d89427555b4d2208dd21101eeba652d0fefecd477f2ea725.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/96cd2f963d315a5154734943951bd0937781c6f7393296bc233a197bddd60a86.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/e2945116339775df2488c01ad45cb9434bb425cedd60a25903b70031215bde04.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/2e0e2461e4f46e45e64b71ba1d60efdf6901dc9a37ed2ee58682286c60f6438f.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/3bdc22da3ec057ee0c6129099900719a2eb4adaba50c31149210db5cdb1e6a0c.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/5a547e77ab0522ceb5868b6b22ea909419438e7329aca24182306684c837ac71.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/68ebca85482ba58965e5651c2c6d94a1427544f45cc5735d2475d77dbf07e1ca.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/a0de8d29d1ac3e400aad4b7d00641f70ae60f4157bdce72c1058008264b76b68.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/073f5324f48199c3a5aeb558d9876a42463efe3d455ca3a35d3261f3820238f6.png'
+];
+
+//魔种
+const mozhong = [
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/3a377db1073694a535ea3c4012aed6ea56ddc61392bf96453b7340c8bb71fa9d.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/d5feab14760b75f3aa450820b88975c5e92c04cfab7ba06243febc4be75d4848.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/2f6d6bb46e5904daf562d426c0ad5d96caf8ddcb43dbc7bcad198b599ba94d17.png'
+]
+
+//混调师
+const huntiao = [
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/2bbe585e8280e61cc88c75074d1a0578986e0c2054a0172521969ae57d46df29.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/193185f0e9d83b3a57523a66744d8adbce53901398c4007d7b4e315e71f6e5e0.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/e8b08827d2888f53c4d5ae7a038d95d3fa73ad56deacef4929b53d8bbddbf55b.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/861ea95c7be087d6d05253681fa515d05be60417db5ab6a1656f831ce46a14ae.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/9355687c520db798229b9feff14ff02dd9e884fe5415e2a483dbf6591abcc5da.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/c52eb9e60009838495a001fc2465ddc176f91b9d87a33455d02369ddd74d9afd.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/410641a67246cc3b6c36332840ba8e43f600d8c8dee7ce03d4d1a30ae33fb99a.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/c32ac56fafb7e68315dc365246b476c8306ddbb72e96dd251b1c34cf5d45ee38.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/ce489677179a09e19f9896e968225e2700beb5e87e2455771f8905a45d3b69fa.png',
+    'https://hs.res.netease.com/pc/zt/20250225182549/static/img/89be10b147956d1b7c0167f5bdefea1d6dabfdebd709f83cec7a9783548d2325.png',
+]
+
+//随机数，包括min和max
+function randownInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//获取当前页面名字，比如company.html
+function currentPage() {
+  let arr = window.location.pathname.split('/')
+  return arr[arr.length-1]
+}
+
+function optionsSelectionInit(list) {
+    console.log("optionsSelectionInit");
+    const select = document.getElementById('option');
+    select.innerHTML = ''
+    list.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item;
+        option.text = item;
+        select.add(option);
+    });
+}
+
+
+function randdomImgDiv(list) {
+    let j = randownInt(0, list.length-1);
+    return `<div class="col-3">
+        <img src="${list[j]}" class="img-fluid" alt="${list[j]}">
+    </div>`
+}
+
+//发现变化，一般是3个，特殊有些是4个，这里直接给4个
+function findChange(value) {
+    let imgHtml = ''
+    switch (value) {
+        case '黑暗之赐':
+            for (let i = 1; i <= 4; i++) {
+                imgHtml += randdomImgDiv(darkness);
+            }
+            break;
+        default:
+            console.log("findChange wrong value:", value);
+            break;
+    }
+
+    document.getElementById("imageContainer").innerHTML = imgHtml;
+}
+
+//召唤变化，每次召唤一个
+function callChange(value) {
+    let imgHtml = ''
+    switch (value) {
+        case '奥秘':
+            imgHtml += randdomImgDiv(mystery);
+            break;
+        case '魔种':
+            imgHtml += randdomImgDiv(mozhong);
+            break;
+        default:
+            console.log("callChange wrong value:", value);
+            break;
+    }
+
+    document.getElementById("imageContainer").innerHTML = imgHtml;
+}
+
+function randomNumberChange(value) {
+    // 生成1到value之间的随机整数（包含1和10）
+    const min = 1;
+    const max = parseInt(value);
+    const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById('alert').innerText = `随机选中了：${randomInt}`;
+}
+
+// 操作类型变化
+function operationChange(event) {
+    console.log("operationChange");
+    const operation = event.target.value;
+    switch (operation) {
+        case 'randomNumber':
+            optionsSelectionInit([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+            break;
+        case 'find':
+            optionsSelectionInit(['黑暗之赐']);
+            break;
+        case 'call':
+            optionsSelectionInit(['奥秘', '魔种']);
+            break;
+        default:
+            console.log("operationChange wrong operation:", operation);
+            break;
+    }
+}
+
+function execute() {
+    console.log("execute");
+    const operation = document.getElementById('operation').value;
+    switch (operation) {
+        case 'randomNumber':
+            randomNumberChange(document.getElementById('option').value);
+            break;
+        case 'find':
+            findChange(document.getElementById('option').value);
+            break;
+        case 'call':
+            callChange(document.getElementById('option').value);
+            break;
+        case 'debug':
+            break;
+        default:
+            console.log("execute wrong operation:", operation);
+            break;
+    }
+}
+
+
+function indexPageInit() {
+    document.getElementById('operation').addEventListener('change', operationChange);
+    document.getElementById('btn-execute').addEventListener('click', execute);
+}
+
+function cardPageInit() {
+    document.getElementById('operation').addEventListener('change', operationChange);
+    document.getElementById('btn-execute').addEventListener('click', execute);
+}
+
+function main() {
+  switch (currentPage()) {
+    case 'index.html':
+        indexPageInit();
+        break
+    case 'card.html':
+        cardPageInit();
+        break
+    default:
+        log('wrong page', currentPage())
+        break
+  }
+}
